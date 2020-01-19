98b64dda3979ee6d1fc6c0406384898edcaaf6e5
Shuapark <shuapark928@gmail.com>
2020-01-19 22:21:35 +0900

    UI part1

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Dimensions, Platform } from 'react-native';
+
+const {height, width} = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar barStyle = "light-content"/>
+      <Text style={styles.title}> To Do List :)</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder={"New To DO"}></TextInput>
+
+      </View>
     </View>
   );
 }
@@ -12,8 +19,45 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F23657',
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
+  title: {
+    color: "white",
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30
+  },
+  card: {
+    backgroundColor: "white",
+    flex: 1,
+    width: width -50,
+    borderTopLeftRadius: 10, //only top&left - radius 10
+    borderTopRightRadius: 10, //Top&Right
+    //platform specific code
+    //ios
+    /*
+    shadowColor
+    shadowOffset
+    shadowOpacity
+    shadowRadius
+    */
+   //Android - elevation : 0-5
+   ...Platform.select({
+     ios: {
+       shadowColor: "rgb(50,50,50)",
+       shadowOpacity: 0.5,
+       shadowRadius: 5,
+       shadowOffset: {
+         height: -1,
+         width: 0
+       }
+     },
+     android: {
+       elevation: 5
+     }
+   })
+  }
 });
